7e691aa896457d3e4c6c4b485f48218c1f57dfc6
msow3011 <msow3011@gmail.com>
2023-11-24 12:52:38 +0000

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-header('Access-Control-Allow-Methods: HEAD, GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: HEAD, GET, POST, PUT, DELETE');
+// header('Access-Control-Allow-Headers: *');
+// header('Access-Control-Allow-Origin: *');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
